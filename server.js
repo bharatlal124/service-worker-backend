3bcb7bc6bf35e665c61dbd18b37d2cb4d9a377af
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-07-08 11:19:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ setVapidDetails(
 );
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://pwa-react-single-page.netlify.app/'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
   credentials: true
